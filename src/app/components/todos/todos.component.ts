1b35f230fc960b4ea41c8f1985ee3fcf03835511
sasikumar6795 <32613154+sasikumar6795@users.noreply.github.com>
2020-12-08 21:16:34 +0530

    LCO changes

--- src/app/components/todos/todos.component.ts
+++ src/app/components/todos/todos.component.ts
@@ -1,31 +1,34 @@
+import { Todo } from './../../model/Todo';
+import { TodoService } from './../../service/todo.service';
 import { Component, OnInit } from '@angular/core';
-import { TodoService } from 'src/app/service/todo.service';
-import {Todo} from './../../model/Todo'
-import {faTrashAlt} from '@fortawesome/free-solid-svg-icons'
-
+import { faTrashAlt } from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-todos',
   templateUrl: './todos.component.html',
-  styleUrls: ['./todos.component.css']
+  styleUrls: ['./todos.component.css'],
 })
 export class TodosComponent implements OnInit {
-   todos : Todo[];
-  constructor(private todoService : TodoService) { }
-  faTrashAlt =faTrashAlt;
-  
-  deleteTodo(todo: Todo)
-  {
-    this.todoService.deleteTodo(todo);
-  }
+  todos: Todo[];
+  // to use this icon in html
+  faTrashAlt = faTrashAlt;
 
-  changeTodoStatus(todo : Todo)
-  {
-    this.todoService.changeTodo(todo);
-  }
+  // using service to get todo
+  constructor(private todoService: TodoService) {}
+
   ngOnInit(): void {
+    // subscribing to service and setting in properties
     this.todoService.getTodos().subscribe((todos) => {
       this.todos = todos;
     });
   }
 
+  // to mark and unmark the todo
+  changeTodoStatus(todo: Todo) {
+    this.todoService.changeTodo(todo);
+  }
+
+  // to delete a single todo
+  deleteTodo(todo: Todo) {
+    this.todoService.deleteTodo(todo);
+  }
 }
